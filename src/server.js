2133763825e273722af65cc8fd342cfc54184b88
Diego Scavone <55470939+diegoscavone@users.noreply.github.com>
2024-08-29 17:23:38 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ['https://foodeexplorerweb.netlify.app','http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['https://foodeexplorerweb.netlify.app'],
     credentials: true
   }))
 
